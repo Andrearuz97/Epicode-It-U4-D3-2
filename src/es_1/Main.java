ea13473ed152d80f0a64f3c1fd5aea92063bc8a8
Andrea Ruzittu <ruzittu.andrea97@gmail.com>
2023-06-28 15:13:54 +0200

    update

--- src/es_1/Main.java
+++ src/es_1/Main.java
@@ -5,13 +5,13 @@ public class Main {
 		String numeroPari = "42";
 		String numeroDispari = "37";
 
-		System.out.println(numeroPari + " è un numero pari: " + PariDispari.stringaPariDispari(numeroPari));
-		System.out.println(numeroDispari + " è un numero dispari: " + PariDispari.stringaPariDispari(numeroDispari));
+		System.out.println(numeroPari + " è un numero pari? " + PariDispari.stringaPariDispari(numeroPari));
+		System.out.println(numeroDispari + " è un numero pari? " + PariDispari.stringaPariDispari(numeroDispari));
 
-		int anno1 = 2020;
-		int anno2 = 2021;
+		int anno1 = 2023;
+		int anno2 = 2025;
 
-		System.out.println(AnnoBisestile.annoBisestile(anno1)); // Output: true
-		System.out.println(AnnoBisestile.annoBisestile(anno2)); // Output: false
+		System.out.println(AnnoBisestile.annoBisestile(anno1));
+		System.out.println(AnnoBisestile.annoBisestile(anno2));
 	}
 }
